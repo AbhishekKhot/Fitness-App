6a14c898b8004039426fd324b128fb6a3c5999c0
AbhishekKhot <khotabhishek469@gmail.com>
2022-01-22 11:47:25 +0530

    added barchart

--- app/src/main/java/com/example/fitnessapp/ui/fragment/StatisticsFragment.kt
+++ app/src/main/java/com/example/fitnessapp/ui/fragment/StatisticsFragment.kt
@@ -2,20 +2,22 @@ package com.example.fitnessapp.ui.fragment
 
 import android.graphics.Color
 import android.os.Bundle
-import android.service.autofill.CharSequenceTransformation
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.fitnessapp.R
+import com.example.fitnessapp.other.CustomMarkerView
 import com.example.fitnessapp.other.TrackingUtility
 import com.example.fitnessapp.viewmodel.StatisticsViewModel
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.farshidroohi.ChartEntity
 import kotlinx.android.synthetic.main.fragment_statistics.*
 import java.lang.Math.round
-import java.lang.NullPointerException
-import javax.annotation.Nullable
 import kotlin.math.roundToInt
 
 @AndroidEntryPoint
@@ -25,7 +27,7 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         subscribeToObserver()
-
+        setUpBarChart()
     }
 
     private fun subscribeToObserver() {
@@ -56,6 +58,44 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                 tvTotalCalories.text = totalCalories
             }
         })
+        viewModel.runSortedByDate.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val allAvgSpeeds =
+                    it.indices.map { i -> BarEntry(i.toFloat(), it[i].avgSpeedInKMH) }
+                val barChartDataSet = BarDataSet(allAvgSpeeds, "Average speed over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                }
+                barChart.data = BarData(barChartDataSet)
+                barChart.marker =
+                    CustomMarkerView(it.reversed(), requireContext(), R.layout.marker_view)
+                barChart.invalidate()
+            }
+        })
 
+    }
+
+    private fun setUpBarChart() {
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.axisRight.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        barChart.apply {
+            description.text = "Average speed over Time"
+            legend.isEnabled = false
+        }
     }
 }
